5c73a4d64c1f613d1e33d57ca988d9a35cc439b0
kkeizzah <keizzahketh@gmail.com>
2025-02-24 21:02:54 +0300

    Update save.js

--- Cmds/Owner/save.js
+++ Cmds/Owner/save.js
@@ -1,33 +1,23 @@
 module.exports = async (context) => {
-    const { client, m } = context;
-    const textL = m.text.toLowerCase();
-    const quotedMessage = m.msg?.contextInfo?.quotedMessage;
+  const { client, m } = context;
 
-    // Check if the quoted message is valid and if the user is trying to post
-    if (quotedMessage && textL.startsWith(prefix + "post") && !m.quoted.chat.includes("status@broadcast")) {
-        return m.reply("You did not tag a status media to post.");
+  const quotedMessage = m.msg?.contextInfo?.quotedMessage;
+
+  if (quotedMessage && !m.quoted.chat.includes("status@broadcast")) {
+    return m.reply("You did not tag a status media to save.");
+  }
+
+  if (quotedMessage && m.quoted.chat.includes("status@broadcast")) {
+    if (quotedMessage.imageMessage) {
+      let imageCaption = quotedMessage.imageMessage.caption;
+      let imageUrl = await client.downloadAndSaveMediaMessage(quotedMessage.imageMessage);
+      client.sendMessage(m.chat, { image: { url: imageUrl }, caption: imageCaption });
     }
 
-    // Check if the owner is sending the post command and if the quoted message is from a broadcast status
-    if (Owner && quotedMessage && textL.startsWith(prefix + "post") && m.quoted.chat.includes("status@broadcast")) {
-        // Handle image media
-        if (quotedMessage.imageMessage) {
-            let imageCaption = quotedMessage.imageMessage.caption;
-            let imageUrl = await client.downloadAndSaveMediaMessage(quotedMessage.imageMessage);
-
-            // Post the image to the status
-            await client.setStatus({ image: { url: imageUrl }, caption: imageCaption });
-            m.reply("Media has been posted to your status.");
-        }
-
-        // Handle video media
-        if (quotedMessage.videoMessage) {
-            let videoCaption = quotedMessage.videoMessage.caption;
-            let videoUrl = await client.downloadAndSaveMediaMessage(quotedMessage.videoMessage);
-
-            // Post the video to the status
-            await client.setStatus({ video: { url: videoUrl }, caption: videoCaption });
-            m.reply("Media has been posted to your status.");
-        }
+    if (quotedMessage.videoMessage) {
+      let videoCaption = quotedMessage.videoMessage.caption;
+      let videoUrl = await client.downloadAndSaveMediaMessage(quotedMessage.videoMessage);
+      client.sendMessage(m.chat, { video: { url: videoUrl }, caption: videoCaption });
     }
+  }
 };
