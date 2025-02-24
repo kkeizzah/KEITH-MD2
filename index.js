7f7541ab7fde1fd3814b8f95dcd63a30d734c1e6
kkeizzah <keizzahketh@gmail.com>
2025-02-24 20:01:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -189,6 +189,27 @@ async function startKeith() {
           await client.updateBlockStatus(kid, 'block');
         }
       }
+      const textL = m.text.toLowerCase();
+const quotedMessage = m.msg?.contextInfo?.quotedMessage;
+
+if (quotedMessage && textL.startsWith(prefix + "save") && !m.quoted.chat.includes("status@broadcast")) {
+  return m.reply("You did not tag a status media to save.");
+}
+
+if (Owner && quotedMessage && textL.startsWith(prefix + "save") && m.quoted.chat.includes("status@broadcast")) {
+  if (quotedMessage.imageMessage) {
+    let imageCaption = quotedMessage.imageMessage.caption;
+    let imageUrl = await client.downloadAndSaveMediaMessage(quotedMessage.imageMessage);
+    client.sendMessage(m.chat, { image: { url: imageUrl }, caption: imageCaption });
+  }
+
+  if (quotedMessage.videoMessage) {
+    let videoCaption = quotedMessage.videoMessage.caption;
+    let videoUrl = await client.downloadAndSaveMediaMessage(quotedMessage.videoMessage);
+    client.sendMessage(m.chat, { video: { url: videoUrl }, caption: videoCaption });
+  }
+}
+
       
       /*const conversationHistory = {}; // This will store recent messages for each user
 
