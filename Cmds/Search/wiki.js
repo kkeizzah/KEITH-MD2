ca501082e040071f9c0e4fedc9bddfc35587c2ae
kkeizzah <keizzahketh@gmail.com>
2025-02-26 00:45:50 +0300

    Update wiki.js

--- Cmds/Search/wiki.js
+++ Cmds/Search/wiki.js
@@ -1,25 +1,31 @@
-module.exports = async (context) => {
-
-const { client, m, text } = context;
-
-
-
 const wiki = require('wikipedia');
+const { sendReply, sendMediaMessage } = require(__dirname + "/../../lib/context"); //
 
-        try {
-            if (!text) return m.reply(`Provide the term to search,\nE.g What is JavaScript!`)
-            const con = await wiki.summary(text);
-            const texa = `Title:- ${con.title}
+module.exports = async (context) => {
+    const { client, m, text } = context;
+
+    try {
+        
+        if (!text) {
+            return sendReply(client, m, `Provide the term to search,\nE.g What is JavaScript!`); 
+        }
+
+        
+        const con = await wiki.summary(text);
+        const texa = `Title:- ${con.title}
                   
 Desc:- ${con.description}
 
 Summary:- ${con.extract}
 
 URL:- ${con.content_urls.mobile.page}
-        `
-            m.reply(texa)
-        } catch (err) {
-            console.log(err)
-            return m.reply(`Got 404. I did not find anything!`)
-        }
+        `;
+
+        
+        await sendReply(client, m, texa); 
+
+    } catch (err) {
+        console.log(err);
+        return sendReply(client, m, `Got 404. I did not find anything!`); 
     }
+};
