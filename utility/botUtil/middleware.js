eddaa3a0fb06459f559783cae8445e050165e7a3
kkeizzah <keizzahketh@gmail.com>
2025-02-26 10:48:40 +0300

    Update middleware.js

--- utility/botUtil/middleware.js
+++ utility/botUtil/middleware.js
@@ -1,24 +1,32 @@
-const { sendReply } = require(__dirname + "/../../lib/context"); 
+
+const { sendReply } = require(__dirname + "/../../lib/context");
+
 
 const middleware = async (context, next) => {
-    const { m, client, isAdmin, isBotAdmin } = context;
+    const { m, client } = context;
 
-    
+    const groupAdmin = m.isGroup ? m.chat.groupMetadata.participants.filter(p => p.admin === 'admin' || p.admin === 'superadmin').map(p => p.id) : [];
+    const botNumber = client.user.jid;
+
+    const isBotAdmins = m.isGroup ? groupAdmin.includes(botNumber) : false;
+    const isAdmins = m.isGroup ? groupAdmin.includes(m.sender) : false;
+
+   
     if (!m.isGroup) {
-        return sendReply(client, m, "This command is meant for groups"); 
+        return sendReply(client, m, "This command is meant for groups");
     }
 
     
-    if (!isAdmin) {
-        return sendReply(client, m, "You need admin privileges is admins"); 
+    if (!isAdmins) {
+        return sendReply(client, m, "You need admin privileges to use this command");
     }
 
     
-    if (!isBotAdmin) {
-        return sendReply(client, m, "I need admin privileges is bot admin"); 
+    if (!isBotAdmins) {
+        return sendReply(client, m, "I need admin privileges to execute this command");
     }
 
-    
+   
     await next();
 };
 
