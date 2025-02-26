158aae0858263f2c46d74200d2802925cbc71538
kkeizzah <keizzahketh@gmail.com>
2025-02-26 10:36:30 +0300

    Update middleware.js

--- utility/botUtil/middleware.js
+++ utility/botUtil/middleware.js
@@ -1,24 +1,24 @@
-const { sendReply } = require(__dirname + "/../../lib/context"); // Import sendReply from context.js
+const { sendReply } = require(__dirname + "/../../lib/context"); 
 
 const middleware = async (context, next) => {
-    const { m, client } = context;
+    const { m, client, isAdmin, isBotAdmin } = context;
 
-    // Check if the message is from a group
+    
     if (!m.isGroup) {
-        return sendReply(client, m, "This command is meant for groups"); // Use sendReply for text replies
+        return sendReply(client, m, "This command is meant for groups"); 
     }
 
-    // Check if the user is an admin
-    if (!m.isAdmin) {
-        return sendReply(client, m, "You need admin privileges is admins"); // Use sendReply for text replies
+    
+    if (!isAdmin) {
+        return sendReply(client, m, "You need admin privileges is admins"); 
     }
 
-    // Check if the bot is an admin
-    if (!m.isBotAdmin) {
-        return sendReply(client, m, "I need admin privileges is bot admin"); // Use sendReply for text replies
+    
+    if (!isBotAdmin) {
+        return sendReply(client, m, "I need admin privileges is bot admin"); 
     }
 
-    // Proceed to the next function (main handler)
+    
     await next();
 };
 
