cc1b0e787d67eeac2c033c1c5f501d329eddbd7b
kkeizzah <keizzahketh@gmail.com>
2025-02-26 11:03:56 +0300

    Update middleware.js

--- utility/botUtil/middleware.js
+++ utility/botUtil/middleware.js
@@ -1,37 +1,26 @@
+
 const { sendReply } = require(__dirname + "/../../lib/context");
 
+
 const middleware = async (context, next) => {
-    const { m, client } = context;
-    
-    // Ensure that the message is from a group
+    const { m, client, isAdmin, isBotAdmin } = context;
+
+  
     if (!m.isGroup) {
-        return sendReply(client, m, "This command is meant for groups.");
+        return sendReply(client, m, "This command is meant for groups");
     }
 
-    // Get the participants who are admins (either 'admin' or 'superadmin')
-    const groupAdmins = m.chat.groupMetadata.participants
-        .filter(p => p.admin === 'admin' || p.admin === 'superadmin')
-        .map(p => p.id);
-
-    const botNumber = client.user.jid;
-
-    // Check if the bot is an admin in the group
-    const isBotAdmin = groupAdmins.includes(botNumber);
-
-    // Check if the sender is an admin in the group
-    const isSenderAdmin = groupAdmins.includes(m.sender);
-
-    // Check if the sender has admin privileges
-    if (!isSenderAdmin) {
-        return sendReply(client, m, "You need admin privileges to use this command.");
+    
+    if (!isAdmin) {
+        return sendReply(client, m, "You need admin privileges to use this command");
     }
 
-    // Check if the bot has admin privileges
+    
     if (!isBotAdmin) {
-        return sendReply(client, m, "I need admin privileges to execute this command.");
+        return sendReply(client, m, "I need admin privileges to execute this command");
     }
 
-    // Proceed with the next middleware or command handler
+   
     await next();
 };
 
